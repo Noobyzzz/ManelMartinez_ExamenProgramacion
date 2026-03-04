4f79bdffaaa6b1e3b53a3eb208c579a5d6ddc4c5
obinyx <manel070807@gmail.com>
2026-03-04 10:18:31 +0100

    hola

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+private Rigidbody2D Rbody2D;
+private InputAction MoveAction;
+
+public Vector2 moveDirection;
+public float movementSpeed = 5f;
+public float jumpForce = 10f;
+private InputAction jumpAction;
+
+private GroundSensor sensor;
+private SpriteRenderer renderer;
+
+    void Awake()
+    {
+        Rbody2D = GetComponent<Rigidbody2D>();
+
+        MoveAction = InputSystem.actions["move"];
+        jumpAction = InputSystem.actions["jump"];
+
+        sensor.GetComponentInChildren<GroundSensor>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = MoveAction.ReadValue<Vector2>;
+
+        if(jumpAction.WasPressedThisFrame () && sensor.isGrounded)
+        
+        {
+            Rbody2D.AddForce (Vector2.up * jumpForce, ForcemMode2D.Impulse);
+
+        }
+        
+        if(moveDirection.x > 0)
+        {
+            render.flipX = false;
+        }
+        else if(moveDirection.x < 0)
+        {
+            render.flipX = true;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        Rbody2D.linearVelocity = new Vector2(moveDirection.x * movementSpeed, Rbody2D.linearVelocity.y);
+    }
+
+
+
+
+}
+
+        
+
+        
+
+
